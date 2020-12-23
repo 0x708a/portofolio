8caf75f09370397fb19aa87fae3990552fae9195
lorddragon386 <46794053+lorddragon386@users.noreply.github.com>
2020-12-23 20:26:19 +0100

    Add files via upload

--- browse/app.js
+++ browse/app.js
@@ -1,5 +1,6 @@
 const p1 = document.querySelector(".p1");
 const p2 = document.querySelector(".p2");
+const p3 = document.querySelector(".p3");
 const back = document.querySelector(".back");
 
 back.addEventListener("click", () => {
@@ -11,5 +12,9 @@ p1.addEventListener("click", () => {
 })
 
 p2.addEventListener("click", () => {
-    window.location.href = "/browse/particleAnimation";
+    window.location.href = "/browse/particleAnimation/";
+})
+
+p3.addEventListener("click", () => {
+    window.location.href = "/browse/threeJScube/";
 })
---
+++ browse/threeJScube/app.js
@@ -0,0 +1,10 @@
+const visit = document.querySelector(".download");
+const back = document.querySelector(".back");
+
+visit.addEventListener("click", () => {
+    window.location.href = "https://rotatingcube.tk"
+})
+
+back.addEventListener("click", () => {
+    window.location.href = "/browse/"
+})
